dcfe488de5e5eca0174d8b1ba8574abd4dd784eb
daniel <buzzy.daniel07@gmail.com>
2024-10-09 00:03:51 -0300

    Implementação das redes sociais no início

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 
 dependencies {
 
+    implementation ("androidx.recyclerview:recyclerview:1.2.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -49,4 +50,5 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.google.code.gson:gson:2.8.9")
+
 }
--- app/src/main/java/com/example/login/Inicio.java
+++ app/src/main/java/com/example/login/Inicio.java
@@ -1,9 +1,11 @@
 package com.example.login;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,14 +16,29 @@ public class Inicio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicio);
 
+        ImageButton buttonFace = findViewById(R.id.imageFace);
+        ImageButton buttonInsta = findViewById(R.id.imageInsta);
         Button inicioButton = findViewById(R.id.btn_inicio);
+
+        buttonFace.setOnClickListener(v -> {
+            String url = "https://https://www.facebook.com";
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+        });
+
+        buttonInsta.setOnClickListener(v -> {
+            String url = "https://https://www.instagram.com";
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+        });
 
         inicioButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Inicio.this, MainActivity.class);
                 startActivity(intent);
-
 
             }
         });
